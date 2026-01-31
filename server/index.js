cb1ced541c5bddb750af758a4b6f10e2db77db95
unnijsx <unniytman@gmail.com>
2026-01-31 15:01:59 +0530

    fix: Update wildcard route syntax for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Enable pre-flight for all routes
+app.options('(.*)', cors(corsOptions)); // Enable pre-flight for all routes
 app.use(express.json());
 
 // Routes
